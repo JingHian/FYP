6c0b5eadeda9dd5e9905b5506167e4189ca301a4
JingHian <61559514+JingHian@users.noreply.github.com>
2022-06-15 21:32:26 +0800

    changed bar colors

--- js/main.js
+++ js/main.js
@@ -24,23 +24,14 @@ const myChart = new Chart(ctx, {
     data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
+            barThickness: 26,
             label: 'water usage',
             data: [12, 22, 3, 5, 2, 3, 22, 3, 5, 2, 3,14],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(54, 162, 235, 0.8)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                  'rgba(54, 162, 235, 0.8)',
             ],
             borderWidth: 1
         }]
