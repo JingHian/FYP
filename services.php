df0f41855a6f4db8f4cd6fe2ea82503a29d4f61d
JingHian <61559514+JingHian@users.noreply.github.com>
2022-06-17 00:30:25 +0800

    changed color for services menu
    changed color for services menu

--- services.php
+++ services.php
@@ -18,14 +18,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
   <?php include_once('navbar.php');?>
-  <h1 class ="display-6 text-center" style="margin-top:20px;margin-bottom:20px;">Track Water Usage</h1>
+  <h1 class ="display-6 text-center " style="margin-top:20px;margin-bottom:20px;">Track Water Usage</h1>
   <div class="container">
     <div class="row ">
     <div class="col-2"></div>
       <div class="d-flex align-items-center justify-content-center col-2 border border-secondary border-3 rounded pt-3 mb-3 height-200">
         <div class =" text-center">
-          <span class="material-symbols-outlined icon-size">water_drop</span>
-          <p class="usage-font ">Water Usage </p>
+          <span class="material-symbols-outlined  font-deepskyblue icon-size">water_drop</span>
+          <p class="usage-font font-deepskyblue">Water Usage </p>
         </div>
       </div>
       <div class="col-1"></div>
