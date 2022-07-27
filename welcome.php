39788a3c2b69aca6f024855a029d228ce49745a5
iansutarji <61403023+iansutarji@users.noreply.github.com>
2022-07-27 21:48:58 +0800

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -4,7 +4,26 @@ session_start();
 // Check if the user is logged in, if not then redirect him to login page
 
 include_once "logInCheck.php";
+include_once "conn.php";
 // echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
+echo "<title>Welcome</title>";
+
+$companyName = $_SESSION["name"];
+$companyId = $_SESSION["ID"];
+$userType = $_SESSION["user_type"];
+
+$getVerifiedStatus = mysqli_query($conn, "select `verified` from company where `company_ID` = '$companyId'");
+$verifiedStatus = $getVerifiedStatus->fetch_array()[0] ?? '';
+
+if ($verifiedStatus == 0 && $userType == 'company') {
+  echo "<h1>Hello $companyName. Your account will be verified by the admin team soon. </h1><br> 
+  <h3>You will be able to use your account after we approve your account creation request.</h3> 
+  <h4>We will send you an email if we have approved or rejected your account verification.</h4>";
+  echo "<a href='logout.php'>log out</a>";
+  //echo $userType;
+
+} else if (($verifiedStatus == 1 && $userType == 'company') || $userType != 'company') {
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +31,7 @@ include_once "logInCheck.php";
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php include_once('cssLinks.php');?>
-    <title>Welcome</title>
+    
 </head>
 <body>
     <?php include_once('navbar.php');?>
@@ -22,3 +41,12 @@ include_once "logInCheck.php";
   <?php include_once('jsLinks.php');?>
 </body>
 </html>
+<?php } else if ($verifiedStatus == 2 && $userType == 'company') {
+  echo "We are sorry that yoru account verification request has been rejected. Please contact the admin for more details.";
+  echo "<a href='logout.php'>log out</a>";
+}
+
+// echo '<pre>';
+// var_dump($_SESSION);
+// echo '</pre>';
+// echo "<a href='logout.php'>log out</a>";?>
